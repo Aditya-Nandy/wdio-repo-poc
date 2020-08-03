4c4947631b68f460b31a7d5f75dcb38a2ef5e2b4
Aditya Nandy <aditya.nandy@cognizant.com>
2020-08-03 15:27:56 +0530

    update wdio conf

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,20 +3,37 @@ const { default: AllureReporter } = require('@wdio/allure-reporter');
 const allureReporter = require('@wdio/allure-reporter').default;
 
 
+
 /*===================== Set globals here ============================== */
-const Env = 'stage';
-const singleTestTimeOut = 60000;
+const Env = process.env.ENV;        // set ENV=stage && npx wdio --spec=login.js
+let Debug = process.env.DEBUG === 'true';   //set ENV=stage&& set DEBUG=true&& npx wdio
+console.log(`Debug mode is set: ${Debug}`);
+const singleTestTimeOut = Debug ? 999999 : 60000;
 const generalElementTimeout = 20000;
 const connectionLostTimeout = 60000;
 const retryTestCount = 0;
 const threads = 3;
+var setCapabilities= [];
 
-/*===================== ***************** ============================== */
-
-
-
+/*===================== Set browser capabilities here ============================== */
 
 
+
+
+const allCapabilities  = 
+[
+    {
+        maxInstances: threads,
+        browserName: 'chrome',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+           // args: ['--incognito']
+            // to run chrome headless the following flags are required
+            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+            // args: ['--headless', '--disable-gpu'],
+        }
+    }
+]
 
 if(!Env || !envData.environment_params.hasOwnProperty(Env)){
     console.error("Environment Setiings mismatch..");
@@ -26,30 +43,20 @@ else{
     envData.Env = Env;
     console.log("Environment is set: "+envData.Env);
 }
+if(Debug){
+    setCapabilities.push(allCapabilities[0]); // set to chrome
+}
+else{
+    setCapabilities = allCapabilities;
+}
 
 exports.config = {
-    //
-    // ====================
-    // Runner Configuration
-    // ====================
-    //
-    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
-    // on a remote machine).
     port: 5555,
     path: '/',
     runner: 'local',
-    //
-    // ==================
-    // Specify Test Files
-    // ==================
-    // Define which test specs should run. The pattern is relative to the directory
-    // from which `wdio` was called. Notice that, if you are calling `wdio` from an
-    // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
-    // directory is where your package.json resides, so `wdio` will be called from there.
-    //
     specs: [
-        // './testscenarios/Dashboard/**/*.js',
-        './testscenarios/Portfolio List/**/*.js'
+        './testscenarios/Dashboard/**/*.js',
+        // './testscenarios/Portfolio List/**/*.js'
     ],
     suites: {
         regression:[
@@ -63,58 +70,12 @@ exports.config = {
     exclude: [
         './testscenarios/reusables/**'
     ],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-    // files and you set maxInstances to 10, all spec files will get tested at the same time
-    // and 30 processes will get spawned. The property handles how many capabilities
-    // from the same test should run tests.
-    //
+
     maxInstances: threads,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://docs.saucelabs.com/reference/platforms-configurator
-    //
-    capabilities: [
-        {
+    services: ['chromedriver'],
+    capabilities: setCapabilities,
     
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
-            maxInstances: threads,
-            browserName: 'chrome',
-            acceptInsecureCerts: true,
-            'goog:chromeOptions': {
-               // args: ['--incognito']
-                // to run chrome headless the following flags are required
-                // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-                // args: ['--headless', '--disable-gpu'],
-            }
-            // If outputDir is provided WebdriverIO can capture driver session logs
-            // it is possible to configure which logTypes to include/exclude.
-            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-            // excludeDriverLogs: ['bugreport', 'server'],
-        }//,
-        // {
-        //     browserName: 'internet explorer'
-        // }
-],
-    //
-    // ===================
-    // Test Configurations
-    // ===================
     // Define all options that are relevant for the WebdriverIO instance here
-    //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'error',
     //
@@ -135,49 +96,12 @@ exports.config = {
     // If you only want to run your tests until a specific amount of tests have failed use
     // bail (default is 0 - don't bail, run all tests).
     bail: 0,
-    //
-    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-    // gets prepended directly.
-
-    //set from run environment
     baseUrl: envData.environment_params[Env].url,
-    //
-    // Default timeout for all waitFor* commands.
     waitforTimeout: generalElementTimeout,
-    //
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
     connectionRetryTimeout: connectionLostTimeout,
-    //
-    // Default request retries count
     connectionRetryCount: retryTestCount,
-    //
-    // Test runner services
-    // Services take over a specific job you don't want to take care of. They enhance
-    // your test setup with almost no effort. Unlike plugins, they don't add new
-    // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
-    
-    // Framework you want to run your specs with.
-    // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: https://webdriver.io/docs/frameworks.html
-    //
-    // Make sure you have the wdio adapter package for the specific framework installed
-    // before running any tests.
     framework: 'mocha',
-    //
-    // The number of times to retry the entire specfile when it fails as a whole
-    // specFileRetries: 1,
-    //
-    // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-    // specFileRetriesDeferred: false,
-    //
-    // Test reporter for stdout.
-    // The only one supported by default is 'dot'
-    // see also: https://webdriver.io/docs/dot-reporter.html
-    // reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+
     reporters: ['spec'],
     reporters: [['allure', {
         outputDir: 'allure-results',
@@ -186,18 +110,12 @@ exports.config = {
     
     }]],
 
-    
-    //
-    // Options to be passed to Mocha.
-    // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
         timeout: singleTestTimeOut
     },
-    //
-    // =====
-    // Hooks
-    // =====
+
+//================================================  Hooks  ====================================================================
     // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
     // it and to build services around it. You can either apply a single function or an array of
     // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
